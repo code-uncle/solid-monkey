c1d717ff9b556b9c8cc0d4a89566cede1a394553
Isaac Pek <isaac@Isaacs-Mac-mini.local>
2022-06-03 23:10:55 +0800

    Added code to generate test pdf

--- solid-monkey/solid-gen.js
+++ solid-monkey/solid-gen.js
@@ -1,2 +1,8 @@
-console.log("Tech Form Generator v1.0")
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+const doc = new PDFDocument();
+
+doc.addPage();
+
+doc.pipe(fs.createWriteStream('test.pdf'));
 
